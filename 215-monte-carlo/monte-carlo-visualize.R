6c37ee1bdf83374c58afd76d30223a3d4891728e
Craig Bonsignore <cbonsig@gmail.com>
2017-05-11 21:57:02 -0700

    finish mc-visualize script, add pdf and png plots

--- 215-monte-carlo/monte-carlo-visualize.R
+++ 215-monte-carlo/monte-carlo-visualize.R
@@ -32,9 +32,6 @@ library(tidyverse)           # http://r4ds.had.co.nz/
 
 # file management  ------------------------------------------------------------
 
-#df <- read_csv('./mc-out/open-frame-fatigue-v25mm-9pct-500se508-500eli.mc.csv.ORIGINAL')
-df <- read_csv('./mc-out/open-frame-fatigue-v25mm-9pct-500se508-500eli.mc.csv')
-
 # set working directory to location of this script
 # note: this is problematic because in RStudio, the default working directory
 # is the project folder, and this is one level down (so we will suffer from
@@ -43,16 +40,38 @@ df <- read_csv('./mc-out/open-frame-fatigue-v25mm-9pct-500se508-500eli.mc.csv')
 # http://stackoverflow.com/questions/13672720/r-command-for-setting-working-directory-to-source-file-location
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
 
+# read results from monte-carlo runs
+# in this case, 500 runs for each material
+df <- read_csv('./mc-out/open-frame-fatigue-v25mm-9pct-500se508-500eli.mc.csv')
+
+# identification to include in plot file names
+file_ident <- 'mc500'
+
+# create folders for results if they do not already exist
+dir.create('mc-pdf', showWarnings = FALSE)
+dir.create('mc-png', showWarnings = FALSE)
+
+
+# helper functions  -----------------------------------------------------------
+
 # function to save file as PDF and PNG
 # PDF files are generally preferred, but
 # get large and unwieldy with many points are plotted
 savePdfPng <- function(name){
-  ggsave(paste('pdf/',ident,'-',name,'.pdf',sep=''),
+  ggsave(paste('mc-pdf/',file_ident,'-',name,'.pdf',sep=''),
          width=8,height=6)
-  ggsave(paste('png/',ident,'-',name,'.png',sep=''),
+  ggsave(paste('mc-png/',file_ident,'-',name,'.png',sep=''),
          width=8,height=6,dpi = 300)
 }
 
+# organize data for plotting  -------------------------------------------------
+
+# rearrange data frame, gathering values spead across multiple columns
+# so there is one value per row. "ident" is the name of the original column
+# from which the value was taken, and "value" is the value itself.
+# add column "orient" containing the r/theta/z orientation
+# add column suffix containing the descriptor (q99 = 99th percentile,
+# q50 = 50th percentile, etc)
 
 df <- df %>%
   arrange(matl,ident) %>%
@@ -64,7 +83,12 @@ df <- df %>%
   mutate(orient = substr(key,1,regexpr("\\.",key)[1]-1),
          suffix = substr(key,regexpr("\\.",key)[1]+1,nchar(key)))
 
-df.tidy <- df %>%
+# spread the data back out across columns
+# now organized with columns to identify the orientation and descriptor
+# some values are not calculated, and appear as zeros...
+# change zeros to NA to make this clear
+
+df <- df %>%
   spread(suffix,value) %>%
   select(-key) %>%
   group_by(ident,matl,run,orient) %>%
@@ -78,47 +102,43 @@ df.tidy <- df %>%
          k     = ifelse(k==0,NA,k),
          q50   = ifelse(q50==0,NA,q50),
          q99   = ifelse(q99==0,NA,q99))
-  #left_join(df,df,by=c('ident','matl','run'))
 
-df <- df.tidy
+# plot the results  -----------------------------------------------------------
 
-# summary <- df %>%
-#   summarise(n = n(),
-#             dK11.mean = mean(dK11.dK),
-#             dK22.mean = mean(dK22.dK),
-#             dK33.mean = mean(dK33.dK),
-#             dK11.max = max(dK11.dK),
-#             dK22.max = max(dK22.dK),
-#             dK33.max = max(dK33.dK))
-
-# also: add qplot(x=k,y=dk)
-
+# for relabeling the orientation labels
 coords <- c(`dK11`='r',
             `dK22`='theta',
             `dK33`='Z')
 
-lucky <- ggplot(data=filter(df,orient=='dK33'),aes(x=D,y=dS,color=dK)) +
-  geom_point(alpha=1/2) +
-  facet_grid(matl ~ .) +
-  scale_colour_gradientn(colours = rev(RColorBrewer::brewer.pal(5,'Spectral'))) +
-  xlab(expression(paste('d',` (`,mu,'m',`)`))) +
-  ylab(expression(paste(Delta,sigma,` (MPa)`))) +
-  labs(color = expression(paste(Delta,'K'))) +
-  ggtitle('max. delta stress intensity factor, by cyclic stress and defect (inclusion) size',
-          'unlucky combination at upper right: largest defect at highest stress')
-plot(lucky)
-
+# plot max(dK) vs max(K@max(dK)) for each orientation
+# this shows 1) the Z orientation is most critical in this case
+# and 2) 
 dK.vs.K <- ggplot(data=filter(df,orient=='dK11'|orient=='dK22'|orient=='dK33'),
-               aes(x=k,y=dK,color=matl)) +
-  geom_point() +
+                  aes(x=k,y=dK,color=matl)) +
+  geom_point(alpha=1/4) +
   facet_grid(. ~ orient, labeller = as_labeller(coords)) +
   ylab(expression(paste(Delta,'K'))) +
   xlab('K') +
   labs(color = 'material') +
-  ggtitle('maximum stress intensity factor and delta stress intensity factor',
+  ggtitle(expression(paste('stress intensity factor: maximum ',Delta,'K and corresponding K')),
           'for 500 monte carlo runs with each material')
 plot(dK.vs.K)
+savePdfPng('dK-vs-K')
 
+# "fortune plot" of dK's components, delta stress and defect size
+fortune <- ggplot(data=filter(df,orient=='dK33'),aes(x=D,y=dS,color=dK)) +
+  geom_point(alpha=1/2) +
+  facet_grid(matl ~ .) +
+  scale_colour_gradientn(colours = rev(RColorBrewer::brewer.pal(5,'Spectral'))) +
+  xlab(expression(paste(d[xy],` (`,mu,'m',`)`))) +
+  ylab(expression(paste(Delta,sigma[z],` (MPa)`))) +
+  labs(color = expression(paste(Delta,K[z]))) +
+  ggtitle(expression(paste('max. ',Delta,K[z], ' by cyclic stress and defect (inclusion) size')),
+          'unluckiest combination at upper right: largest defect at highest cyclic stress')
+plot(fortune)
+savePdfPng('fortune')
+
+# histogram: maximum K for each orientation
 max.K <- ggplot(data=filter(df,
                             orient=='dK11'|
                               orient=='dK22'|
@@ -130,7 +150,9 @@ max.K <- ggplot(data=filter(df,
   ggtitle('maximum stress intensity factor',
           'for 500 monte carlo runs with each material')
 plot(max.K)
+savePdfPng('max-K')
 
+# histogram: maximum dK for each orientation
 max.dK <- ggplot(data=filter(df,
                            orient=='dK11'|
                              orient=='dK22'|
@@ -142,25 +164,14 @@ max.dK <- ggplot(data=filter(df,
   ggtitle('maximum delta stress intensity factor',
           'for 500 monte carlo runs with each material')
 plot(max.dK)
+savePdfPng('max-dK')
 
+# max dK vs defect (inclusion) size
 max.dK.v.D <- ggplot(data=filter(df,orient=='dK33'),aes(x=D,y=dK,color=matl)) +
   geom_point(alpha=1/5) +
-  xlab(expression(paste('d',` (`,mu,'m',`)`))) +
-  ylab(expression(paste(Delta,'K',` (MPa`,sqrt(m),')'))) +
+  xlab(expression(paste(d[xy],` (`,mu,'m',`)`))) +
+  ylab(expression(paste(Delta,K[z],` (MPa`,sqrt(m),')'))) +
   ggtitle('maximum stress intensity factor depends strongly on defect (inclusion) size',
           '500 monte carlo runs with each material')
 plot(max.dK.v.D)
-
-
-# maximum dK vs 99th, 50th pctl dK
-# measure of the "extremity" of defect size
-# maximum value is in the ranege 1.5x to >2x 99th pctl value
-q.99 <- ggplot(data=filter(df,orient=='dK33'),aes(x=q99,y=dK)) +
-  geom_point(aes(color=matl),alpha=1/5) +
-  ggtitle('q.99')
-plot(q.99)
-
-q.50 <- ggplot(data=filter(df,orient=='dK33'),aes(x=q50,y=dK)) +
-  geom_point(aes(color=matl),alpha=1/5) +
-  ggtitle('q.50')
-plot(q.50)
+savePdfPng('max-dK-D')
